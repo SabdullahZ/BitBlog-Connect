54e638a52000f4744f7558f7a692baa0c596ed2f
Shehrozfazal <89447388+shahrozfazal@users.noreply.github.com>
2024-06-04 23:52:24 +0500

    Initial commit

---
+++ backend/dto/blog-details.js
@@ -0,0 +1,11 @@
+class BlogDetailsDTO {
+  constructor(blog) {
+    this._id = blog._id;
+    this.content = blog.content;
+    this.title = blog.title;
+    this.photo = blog.photoPath;
+    this.createdAt = blog.createdAt;
+  }
+}
+
+module.exports = BlogDetailsDTO;
